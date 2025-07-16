e5d431158ded9454938327898c14e4bbf74eb043
Albonire <andersonfabygonzalez.21@gmail.com>
2025-07-16 11:28:25 -0500

    Refactor schedule template to enhance layout and functionality; integrate dynamic content generation, update styles, and add notification feature.

--- tareas/templatetags/task_extras.py
+++ tareas/templatetags/task_extras.py
@@ -2,4 +2,6 @@ from django.template.defaultfilters import register
 
 @register.filter
 def get_item(dictionary, key):
+    if dictionary is None:
+        return None
     return dictionary.get(key)
