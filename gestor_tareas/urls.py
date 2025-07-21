dcc4dbd285ef416d3e34fcb1978b791a9c99f01b
Albonire <andersonfabygonzalez.21@gmail.com>
2025-07-21 16:15:15 -0500

    Update STATIC_URL in settings.py for proper static file handling and add static file URL patterns in urls.py for development. Update database file to reflect changes.

--- gestor_tareas/urls.py
+++ gestor_tareas/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from tareas import views as tareas_views
 
 urlpatterns = [
@@ -24,3 +26,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('tareas.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
