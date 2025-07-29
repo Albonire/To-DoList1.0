d116a3d885f0be1040d8961f5a795519a74bd0eb
Albonire <andersonfabygonzalez.21@gmail.com>
2025-07-28 22:32:47 -0500

    Add ready method in TareasConfig to import signals for proper registration, enhancing application initialization.

--- tareas/apps.py
+++ tareas/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class TareasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tareas'
+
+    def ready(self):
+        # Importa las señales para que se registren correctamente
+        import tareas.signals
